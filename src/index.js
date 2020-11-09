13b4fbbd650a2776441a8b22c7ef1072c7b657ca
bmukha <47007157+bmukha@users.noreply.github.com>
2020-11-09 20:03:43 +0200

    some improvements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import pageLoad from "./pageLoad";
+import loadAbout from "./about";
+import loadContacts from "./contacts";
+import loadMenu from "./menu";
 
-pageLoad();
+const content = document.querySelector("#content");
+
+const about = document.querySelector("#about");
+about.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadAbout(content);
+});
+
+const menu = document.querySelector("#menu");
+menu.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadMenu(content);
+});
+
+const contacts = document.querySelector("#contacts");
+contacts.addEventListener("click", () => {
+    content.innerHTML = "";
+    loadContacts(content);
+});
+
+
+
+
+ loadAbout(content);
